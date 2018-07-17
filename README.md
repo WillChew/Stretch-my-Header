39be0dca89a323854bc141f32825ff800d411f2d
WillChew <williamchew95@gmail.com>
2018-07-17 12:02:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Stretch-my-Header
---
+++ Stretch My Header/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Stretch My Header
+//
+//  Created by Will Chew on 2018-07-17.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
